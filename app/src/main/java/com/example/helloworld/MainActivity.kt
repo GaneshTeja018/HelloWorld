fb6d098267e8d93351416de244b11aacbff4ef3e
Ganeshteja B <bachalateja@gmail.com>
2021-12-02 15:15:52 +0530

    HelloWorld

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var clickCount=0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickCountDisplayView = findViewById<TextView>(R.id.clickCountView)
+        val clickMeButton = findViewById<TextView>(R.id.clickButton)
+
+        clickMeButton.setOnClickListener {
+            clickCount++
+            clickCountDisplayView.setText("clicked $clickCount times")
+        }
+    }
+}
